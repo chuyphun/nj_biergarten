7bd98d06a449f61ecf6540e406a565adb32cb198
chuyphun <abfedcg@gmail.com>
2025-02-06 17:44:38 +0800

    Image.Image is the right type; Image (alone) is a module

--- util.py
+++ util.py
@@ -48,7 +48,7 @@ class TrOCREngine:
     def __repr__(self) -> str:
         return f'TrOCREngine(processor_dir="{self.processor_dir}", model_dir="{self.model_dir}")'
 
-    def crack_captcha(self, image: Image | torch.Tensor | np.ndarray) -> str:
+    def crack_captcha(self, image: Image.Image | torch.Tensor | np.ndarray) -> str:
         pixel_values = self.processor(image, return_tensors='pt').pixel_values
         generated_ids = self.model.generate(pixel_values)
         generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
